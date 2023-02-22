567c427de7ed548c72a1523a742ba9d120e268fa
energee <energee@users.noreply.github.com>
2023-02-22 04:42:08 +0000

    jekyll build from Action 71f286c232fe1ae92495d9835d4b395e80bdd9ac

---
+++ assets/js/render-beer-persist.js
@@ -0,0 +1,60 @@
+new Vue({
+  delimiters: ['${', '}'],
+  el: '#app',
+  data: {
+    items: []
+  },
+  methods: {
+    async loadData(addr) {
+      let rLk = Math.random().toString(36).slice(2, 4);
+      let rLv = Math.random().toString(36).slice(2, 4);
+      try {
+        const response = await fetch(`${addr}?${rLk}=${rLv}`);
+        const data = await response.json();
+        this.items = data;
+        this.$nextTick(this.changeColors)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async refreshOnUpate() {
+      try {
+        let rLk = Math.random().toString(36).slice(2, 4);
+        let rLv = Math.random().toString(36).slice(2, 4);
+        const response = await fetch(`/timestamp.json?${rLk}=${rLv}`);
+        const data = await response.json();
+        let timestamp = data.timestamp;
+        let lastRefresh = localStorage.getItem("lastRefresh");
+        if (timestamp != lastRefresh) {
+          location.reload()
+          localStorage.setItem("lastRefresh", timestamp);
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    adjustPosition(app) {
+      let height = app.offsetHeight;
+      let margin = (window.innerHeight - height) / 2
+      app.style.marginTop = margin + 'px';
+    },
+    changeColors() {
+      let items = document.querySelectorAll('article');
+      for (let i = 0; i < items.length; i++) {
+        let color = randomColor({ luminosity: "light" });
+        items[i].style.color = color;
+      }
+    }
+  },
+  mounted() {
+    const app = document.querySelector("#app");
+    const addr = app.dataset.addr;
+    this.loadData(addr, app);
+    setInterval(() => {
+      this.adjustPosition(app);
+      this.loadData(addr);
+      this.refreshOnUpate();
+    }, 5000);
+    window.addEventListener('error', () => location.reload(), true);
+  }
+});
